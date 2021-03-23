b4ffecb3ba158428a358332a9eaf9f907f07a6df
Elliot <hack00mind@gmail.com>
2021-03-23 22:35:06 +0800

    feat: simple implementation of Signal

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+pub struct Signal {
+    receivers: Vec<Box<dyn Fn(&str) -> ()>>
+}
+
+impl Signal {
+    pub fn new() -> Self {
+        Self { receivers: vec![] }
+    }
+
+    pub fn connect(mut self, f: impl Fn(&str) -> () + 'static) -> Self {
+        self.receivers.push(Box::new(f));
+        self
+    }
+
+    pub fn send(&self, message: &str) {
+        for receiver in self.receivers.iter() {
+            (receiver)(message)
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
