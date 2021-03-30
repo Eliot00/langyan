4ec8a47261f00239fec6daba406fa16edce849eb
Elliot <hack00mind@gmail.com>
2021-03-30 23:04:42 +0800

    feat: extract Receiver trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,87 @@
 pub struct Signal<T: ?Sized> {
-    receivers: Vec<Box<dyn Fn(&T) -> ()>>
+    receivers: Vec<Box<dyn Receiver<T>>>,
 }
 
-impl<T> Signal<T> where T: ?Sized {
+impl<T> Signal<T>
+where
+    T: ?Sized,
+{
     pub fn new() -> Self {
         Self { receivers: vec![] }
     }
 
-    pub fn connect(mut self, receiver: impl Fn(&T) -> () + 'static) -> Self {
+    pub fn connect<R>(mut self, receiver: R) -> Self
+    where
+        R: Receiver<T> + 'static,
+    {
         self.receivers.push(Box::new(receiver));
         self
     }
 
+    pub fn disconnect<R>(&self, _receiver: R)
+    where
+        R: Receiver<T> + 'static,
+    {
+        // Todo
+    }
+
     pub fn send(&self, sender: &T) {
         for receiver in self.receivers.iter() {
-            (receiver)(sender)
+            receiver.handle_signal(sender)
         }
+    }
+}
+
+pub trait Receiver<T: ?Sized> {
+    fn handle_signal(&self, sender: &T);
+}
+
+impl<T, F> Receiver<T> for F
+where
+    F: Fn(&T) -> () + 'static,
+    T: ?Sized,
+{
+    fn handle_signal(&self, sender: &T) {
+        self(sender);
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use mockall::*;
 
     #[test]
     fn should_send_str() {
-        use mockall::*;
-
         #[automock]
-        pub trait Foo {
+        trait Foo {
             fn foo(s: &str);
         }
 
         let ctx = MockFoo::foo_context();
         ctx.expect().once().returning(|s| println!("Sender: {}", s));
 
-        let signal = Signal::new()
-            .connect(MockFoo::foo);
+        let signal = Signal::new().connect(MockFoo::foo);
         signal.send("hello world");
+    }
+
+    #[test]
+    fn test_disconnect() {
+        #[automock]
+        trait One {
+            fn one(s: &str);
+        }
+
+        let ctx = MockOne::one_context();
+        ctx.expect()
+            .once()
+            .returning(|s| println!("Only invoke once: {}", s));
+
+        let signal = Signal::new().connect(MockOne::one);
+        signal.send("first");
+
+        signal.disconnect(MockOne::one);
+
+        signal.send("second");
     }
 }
